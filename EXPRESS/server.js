a4c5330c9150a4d003fb28cbc5c7400f60c24df8
VishalBhagour <vishalbhagour7@gmail.com>
2026-01-19 10:02:29 +0530

    [Vishal Bhagour] add:- EXPRESS (server)

---
+++ EXPRESS/server.js
@@ -0,0 +1,51 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+app.get("/home", (req, res) => {
+    res.send("hello vishal")
+})
+app.get("/about", (req, res) => {
+    res.send("hello dev")
+})
+app.get("/kingkong", (req, res) => {
+    res.send("hello manjeet")
+})
+app.get("/doratheexplorer", (req, res) => {
+    res.send("hello shashank")
+})
+
+app.get("/userdetails1", (req, res) => {
+
+    let user1 = {
+        status: "success",
+        message: "hello from express1",
+        timestamp: new Date().toISOString()
+    }
+    res.json(user1)
+})
+app.get("/userdetails2", (req, res) => {
+
+    let user2 = {
+        status: "success",
+        message: "hello from express2",
+        timestamp: new Date().toISOString()
+    }
+    res.json(user2)
+})
+app.get("/userdetails3", (req, res) => {
+
+    let user3 = {
+        status: "success",
+        message: "hello from express3",
+        timestamp: new Date().toISOString()
+    }
+    res.json(user3)
+})
+
+app.listen(3000, () => {
+    console.log("server is running")
+})
