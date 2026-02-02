8622f7e00957401ae7ac1cfcf5b9703c31de112d
VishalBhagour <vishalbhagour7@gmail.com>
2026-02-02 15:19:23 +0530

    [Vishal bhagour] add:- router project

--- Router/server.js
+++ Router/server.js
@@ -1,18 +1,18 @@
-import express from 'express';
+// import express from 'express';
 
-const port = 3000;
+// const port = 3000;
 
-const app = express();
+// const app = express();
 
-import userRouter from './router/userRoute.js';
+// import userRouter from './router/userRoute.js';
 
-import reg from './router/registration.js';
-import dash from './router/dashboard.js'
+// import reg from './router/registration.js';
+// import dash from './router/dashboard.js'
 
-app.use('/auth', reg);
-app.use('/api', userRouter);
-app.use('/dashboard', dash);
+// app.use('/auth', reg);
+// app.use('/api', userRouter);
+// app.use('/dashboard', dash);
 
-app.listen(port, () => {
-    console.log("Server is running on port " + port);
-});
+// app.listen(port, () => {
+//     console.log("Server is running on port " + port);
+// });
